5db85a243717803fac39dfc1fc81c9b26f7109c7
What Can I Call Myself <WhatCanICallMyself@proton.me>
2024-12-04 09:14:16 +0100

    Added grid class without cell...

---
+++ grid.py
@@ -0,0 +1,17 @@
+import Cell from cell
+
+class Grid:
+    def __init__(self, size: int):
+        self._size = size
+
+        # Create square 2d array of cells
+        # False will be cell...
+        self._grid = [[False for _ in range(size)] for _ in range(size)]
+
+    def __str__(self):
+        return "\n" * self._size + "\n".join("".join(str(cell) for cell in row) for row in self._grid)
+
+    def get_cell(self, x, y):
+        if x < 0 or y < 0 or x >= len(self._grid) or y >= len(self._grid[x]):
+            return None
+        return self._grid[x][y]
