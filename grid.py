30791063abd76a72c3428e72a351b80be79b5d22
What Can I Call Myself <WhatCanICallMyself@proton.me>
2024-12-04 09:49:09 +0100

    Added cell to grid

--- grid.py
+++ grid.py
@@ -1,15 +1,14 @@
-import Cell from cell
+from cell import Cell
 
 class Grid:
     def __init__(self, size: int):
         self._size = size
 
         # Create square 2d array of cells
-        # False will be cell...
-        self._grid = [[False for _ in range(size)] for _ in range(size)]
+        self._grid = [[Cell() for _ in range(size)] for _ in range(size)]
 
     def __str__(self):
-        return "\n" * self._size + "\n".join("".join(str(cell) for cell in row) for row in self._grid)
+        return "\n".join("".join(str(cell) for cell in row) for row in self._grid)
 
     def get_cell(self, x, y):
         if x < 0 or y < 0 or x >= len(self._grid) or y >= len(self._grid[x]):
